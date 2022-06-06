0be06ed7cc497020b14275f74508f1ae144759b5
nefan-k <kaleks@gmx.com>
2022-06-05 22:07:45 -0400

    Add navigation pannel and deletion safeguard

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,6 @@
 class Author < ApplicationRecord
+    validates_presence_of :last_name
+    validates_presence_of :first_name
     has_and_belongs_to_many :books
 
     def full_name
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,4 @@
 class Genre < ApplicationRecord
+    validates_presence_of :name
     has_many :books
 end
